cef3aeec1c209a5ef0c4a6cfb8bbe0dc152c02b6
Nifemi <nifex007@yahoo.com>
2017-10-26 14:55:15 +0100

    function to evaluate I(x)

--- polynomial_calculus.py
+++ polynomial_calculus.py
@@ -1,8 +1,11 @@
 '''
+POLYNOMIAL CALCULUS
+
 INPUT N                     {Degree of P(x)}
 INPUT A(0), A(1)....A(N)    {Coefficients of P(x)}
 INPUT C                     {Constant of integration}
 INPUT X                     {Indepedent Variable}
+INPUT x                     {List of coefficients of the equation}
 '''
 
 import numpy
@@ -26,6 +29,7 @@ def eval_polynomial(x, X):
     B[counter] = A[0] # B(N):=A(N)
     print('b({0}) = {1}'.format(counter, A[0])) 
     counter = counter - 1
+
     for N in range(1,index):
         B[counter] = A[N] + B[counter + 1] * X 
         print('b({0}) = {1} +({2})*{3} = {4}'.format(counter, A[N], B[counter + 1], X, B[counter]))
@@ -44,13 +48,46 @@ def eval_polynomial_derivative(x, X):
     D[counter - 1] = counter * A[0]  # D(N-1) := N * A(N) 
     print("d({0}) = {1} * {2} = {3}".format(counter - 1, counter, A[0], D[counter - 1])); 
     counter = counter - 1  
-    for N in range(1,index):
-        D[counter - 1] = (counter) * A[N] + D[counter] * X #D(K-1):= K * A(K) + D(K) * X
+
+    for N in range(1,index): # For K = N-1  DOWNTO 1
+        D[counter - 1] = (counter) * A[N] + D[counter] * X # DO D(K-1):= K * A(K) + D(K) * X
         print("d({0}) = {1} * {2} + {3} * {4} = {5}".format(counter - 1, counter, A[N], D[counter], X, D[counter - 1])); 
         
         counter = counter - 1
-        
 
+
+'''
+Algorithm to evaluate I(x)
+upper       {Upper limit}
+lower       {Lower limit}
+'''
+def eval_polynomial_integral(x,X,upper,lower):
+    equation = p(x)
+    A = equation.c
+    I = copy.copy(x) 
+    index = len(A) 
+    counter = len(A) - 1
+    
+    I.insert((counter + 1),(A[0] /(counter + 1))) # I(N+1):= A(N)/(N+1)
+    print("I({0}) = {1}/{2} = {3}".format(counter + 1, A[0], counter + 1, I[counter + 1]))
+    
+
+    for N in range(1, index): #For K=N DOWNTO 1
+        I[counter] =( A[N] / counter ) + I[counter + 1] * X # DO I(K):= A(K-1)/K + I(K+1)*X
+        print("I({0}) = {1}/{2} + {3} * {4} = {5}".format(counter, A[N], counter, I[counter + 1], X, I[counter]))
+        counter = counter - 1
+    
+    #   I(0):= C + I(1)*X
+    I[0] = 0 + I[1] * X
+    print("I(0) = 0 + {0} * {1} = {2}".format(I[1], X, I[0]))
+
+    
+
+
+
+
+    
+    
 
 
 
@@ -58,4 +95,5 @@ def eval_polynomial_derivative(x, X):
     
     
 #print(eval_polynomial([-0.02, 0.2, -0.4,1.28], 4))
-print(eval_polynomial_derivative([-0.02, 0.2, -0.4,1.28], 4))
+#print(eval_polynomial_derivative([-0.02, 0.2, -0.4,1.28], 4))
+print(eval_polynomial_integral([-0.02, 0.2, -0.4,1.28], 4, 4,1))
